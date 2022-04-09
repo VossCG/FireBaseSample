e6d1b751cedf990390ec481d3ccf65253e3a3df8
Voss <yot4106@gmail.com>
2022-04-09 13:30:16 +0800

    second commit Firebase GDG 新增email驗證功能

--- app/src/main/java/com/voss/firebase_gdg/LoginFragment.kt
+++ app/src/main/java/com/voss/firebase_gdg/LoginFragment.kt
@@ -1,28 +1,28 @@
 package com.voss.firebase_gdg
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.voss.firebase_gdg.databinding.LoginfragmentBinding
+import java.util.*
 
 class LoginFragment : BaseFragment<LoginfragmentBinding>(LoginfragmentBinding::inflate) {
-    private var userCorrect = false
-    private var isVerified = false
     private val navController by lazy { findNavController() }
     private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onStart() {
+        super.onStart()
         checkLogin()
+        Log.d("LoginFragment","user:${mAuth.currentUser} verified:${mAuth.currentUser?.isEmailVerified}")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
 
         binding.loginBut.setOnClickListener {
             val pair: Pair<String, String>? = getInput()
@@ -31,23 +31,16 @@ class LoginFragment : BaseFragment<LoginfragmentBinding>(LoginfragmentBinding::i
             else Toast.makeText(this.context, "Please filled out the fields", Toast.LENGTH_SHORT)
                 .show()
         }
+
         binding.toSingUpBut.setOnClickListener {
             navController.navigate(R.id.action_loginFragment_to_signUpFragment)
-        }
-    }
-
-    private fun navigateToHome() {
-        if (userCorrect && isVerified) {
-            navController.navigate(R.id.action_loginFragment_to_homeFragment)
-        } else {
-            Toast.makeText(this.context, "信箱未驗證過", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun checkLogin() {
         val user = mAuth.currentUser
         Log.d("Login", user.toString())
-        if (user != null) {
+        if (user != null && user.isEmailVerified) {
             Toast.makeText(this.context, "已有登入紀錄", Toast.LENGTH_SHORT).show()
             navController.navigate(R.id.action_loginFragment_to_homeFragment)
         }
@@ -62,13 +55,39 @@ class LoginFragment : BaseFragment<LoginfragmentBinding>(LoginfragmentBinding::i
         } else null
     }
 
+    // 登入的時候 verified 才變成true ， 不是點選信件完成才更動
     private fun setFireBaseLogin(email: String, password: String) {
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                Toast.makeText(this.context, "login Successful", Toast.LENGTH_SHORT).show()
                 navigateToHome()
             } else
                 Toast.makeText(this.context, task.exception?.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToHome() {
+        // 信箱是否被驗證過
+        if (mAuth.currentUser?.isEmailVerified!!) {
+            Toast.makeText(this.context, "login Successful", Toast.LENGTH_SHORT).show()
+            navController.navigate(R.id.action_loginFragment_to_homeFragment)
+        } else {
+            Toast.makeText(this.context, "信箱未驗證過", Toast.LENGTH_SHORT).show()
+
+            AlertDialog.Builder(this.context)
+                .setTitle("驗證信箱")
+                .setMessage("需驗證完信箱 才能繼續使用 \n 是否現在進行驗證?")
+                .setPositiveButton("Yes") { _, _ ->
+                    sendVerifiedEmail()
+                }.setNegativeButton("No", null)
+                .show()
+        }
+    }
+
+    private fun sendVerifiedEmail() {
+        mAuth.currentUser?.sendEmailVerification()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this.context, "開始驗證 email", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this.context, "驗證過程失敗", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/voss/firebase_gdg/SignUpFragment.kt
+++ app/src/main/java/com/voss/firebase_gdg/SignUpFragment.kt
@@ -1,9 +1,13 @@
 package com.voss.firebase_gdg
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.navigateUp
 import com.google.firebase.auth.FirebaseAuth
 import com.voss.firebase_gdg.databinding.SingupfragmentBinding
 
@@ -22,6 +26,16 @@ class SignUpFragment : BaseFragment<SingupfragmentBinding>(SingupfragmentBinding
         }
     }
 
+//
+//    private fun checkLogin() {
+//        val user = mAuth.currentUser
+//        Log.d("Login", user.toString())
+//        if (user != null && user.isEmailVerified) {
+//            Toast.makeText(this.context, "已有登入紀錄", Toast.LENGTH_SHORT).show()
+//            navController.navigate(R.id.action_loginFragment_to_homeFragment)
+//        }
+//    }
+
     private fun getInputFromEditText(): Pair<String, String>? {
         val email = binding.accountRegisterEdittext.text.toString()
         val password = binding.passwordRegisterEdittext.text.toString()
@@ -35,8 +49,23 @@ class SignUpFragment : BaseFragment<SingupfragmentBinding>(SingupfragmentBinding
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
             if (it.isSuccessful) {
                 Toast.makeText(this.context, "註冊成功", Toast.LENGTH_SHORT).show()
-                navController.navigate(R.id.action_signUpFragment_to_loginFragment)
+                AlertDialog.Builder(this.context)
+                    .setTitle("驗證信箱")
+                    .setMessage("需驗證完信箱 才能登入 \n是否現在進行驗證?")
+                    .setPositiveButton("是,現在進行") { _, _ ->
+                        sendVerifiedEmail()
+                        navController.navigate(R.id.action_signUpFragment_to_loginFragment)
+                    }.setNegativeButton("否,稍後再說", null)
+                    .show()
             } else Toast.makeText(this.context, it.exception?.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendVerifiedEmail() {
+        mAuth.currentUser?.sendEmailVerification()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this.context, "開始驗證 email", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this.context, "驗證過程失敗", Toast.LENGTH_SHORT).show()
         }
     }
 }
